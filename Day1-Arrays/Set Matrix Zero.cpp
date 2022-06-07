18c26ea42d8b2c9823eddabf2ea4a75f186b2614
Shreya Ghosh <72400676+shreya024@users.noreply.github.com>
2022-06-07 06:17:04 +0530

    optimal approach added

--- Day1-Arrays/Set Matrix Zero.cpp
+++ Day1-Arrays/Set Matrix Zero.cpp
@@ -2,6 +2,7 @@
 Given an ‘N’ x ‘M’ integer matrix, if an element is 0, set its entire row and column to 0's, and return the matrix. In particular, your task is to modify it in such a way that if a cell has a value 0 (matrix[i][j] == 0), then all the cells of the ith row and jth column should be changed to 0.
 You must do it in place.*/
 
+//Better Approach
 #include <bits/stdc++.h> 
 void setZeros(vector<vector<int>> &matrix)
 {
@@ -32,6 +33,37 @@ void setZeros(vector<vector<int>> &matrix)
                     matrix[i][j]=0;
                 }
             }
+        }
+    
+}
+
+//Optimal Approach
+#include <bits/stdc++.h> 
+void setZeros(vector<vector<int>> &matrix)
+{
+	int row_size=matrix.size();
+        int col_size=matrix[0].size();
+        
+        bool col=true;
+        for(int i=0;i<row_size;i++){
+            if(matrix[i][0]==0)
+                col=false;
+            for(int j=1;j<col_size;j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                }
+            }
+        }
+
+        for(int i=row_size-1;i>=0;i--){
+            for(int j=col_size-1;j>0;j--){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
+                    matrix[i][j]=0;
+                }
+            }
+            if(col==false)
+                matrix[i][0]=0;
         }
     
 }
