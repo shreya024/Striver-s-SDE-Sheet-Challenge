1271514d5d969f6a284ba411518736867f4427ee
Shreya Ghosh <72400676+shreya024@users.noreply.github.com>
2022-06-07 16:01:22 +0530

    Kadane's Algorithm added

---
+++ Day1-Arrays/Maximum Subarray Sum.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h> 
+long long maxSubarraySum(int arr[], int n)
+{
+    long long ans=-2145678;
+    long long sum=0;
+    for(int i=0;i<n;i++){
+        sum+=arr[i];
+        ans=max(ans,sum);
+        if(sum<0){
+            sum=0;
+        }
+    }
+    return ans;
+}
