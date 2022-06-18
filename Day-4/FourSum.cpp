86eff6cbfc840a2bc5b3206edd9a8fc58a94ba5a
Shreya Ghosh <72400676+shreya024@users.noreply.github.com>
2022-06-18 08:27:41 +0530

    Update FourSum.cpp

--- Day-4/FourSum.cpp
+++ Day-4/FourSum.cpp
@@ -43,3 +43,57 @@ public:
         
     }
 };
+
+//Method 2 ---Nested Loop+Two Pointer + Removing Duplicates
+
+#include <bits/stdc++.h> 
+string fourSum(vector<int> arr, int target, int n) {
+    vector<vector<int>> ans;
+        vector<int> nums=arr;
+        sort(nums.begin(),nums.end());
+        
+        for(int i=0;i<n;i++){
+            long long target3=target-nums[i];
+            for(int j=i+1;j<n;j++){
+                
+                    long long target2=target3-nums[j];
+                    
+                    int left=j+1,right=n-1;
+                    while(left<right){
+                        long long two_sum=nums[left]+nums[right];
+                        
+                        if(two_sum<target2)
+                            left++;
+                        else if(two_sum>target2) 
+                            right--;
+                        else{
+                            vector<int> q;
+                            q.push_back(nums[i]);
+                            q.push_back(nums[j]);
+                            q.push_back(nums[left]);
+                            q.push_back(nums[right]);
+                            ans.push_back(q);
+                            return "Yes";
+                            //Removing duplicate 3rd element
+                            while(left<right && q[2]==nums[left])
+                                    left++;
+
+                            //Removing dupliacte 4th element
+                            while(left<right && q[3]==nums[right])
+                                    right--;
+                        }
+                    }
+                    //Removing duplicate 2nd element
+                    while(j+1<n && nums[j]==nums[j+1])
+                        j++;
+            }
+            //Removing duplicate 1st element
+            while(i+1<n && nums[i]==nums[i+1])
+                i++;
+        }
+        
+        
+        return "No";
+    
+}
+
