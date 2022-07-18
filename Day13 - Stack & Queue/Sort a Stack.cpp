8f5f01a01015a1ad2b3cb43b6fcff8e8f9a9c276
Shreya Ghosh <72400676+shreya024@users.noreply.github.com>
2022-07-18 16:27:40 +0530

    Create Sort a Stack.cpp

---
+++ Day13 - Stack & Queue/Sort a Stack.cpp
@@ -0,0 +1,24 @@
+void sortedInsert(stack<int> &stack,int num){
+    //base case
+    if(stack.empty() || (!stack.empty() && stack.top()<num)){
+        stack.push(num);
+        return;
+    }
+    
+    int n=stack.top();
+    stack.pop();
+    sortedInsert(stack,num);
+    stack.push(n);
+}
+
+void sortStack(stack<int> &stack)
+{
+	// base case
+    if(stack.empty())
+    	return;
+    int num=stack.top();
+    stack.pop();
+    //recursive call
+    sortStack(stack);
+    sortedInsert(stack,num);
+}
